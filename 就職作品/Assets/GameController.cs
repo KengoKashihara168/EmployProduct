28f523663e80f74f1cede3a91b22efab88436cca
KengoKashihara <kengokashihara1998@gmail.com>
2019-05-07 09:21:26 +0900

    カウントダウンUIの作成

---
+++ 就職作品/Assets/GameController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    float limitTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        limitTime = 60.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckTime(limitTime - Time.time);
+    }
+
+    void CheckTime(float time)
+    {
+        if (time >= 0.0f) return;
+
+        Debug.Log("ゲームオーバー");
+    }
+}
