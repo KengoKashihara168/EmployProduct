cc83d9c9ee779070dacf1175cc1a7d1e8c4385bf
KengoKashihara <KengoKashihara1998@gmail.com>
2019-05-08 09:14:30 +0900

    アセットバンドルの導入

--- 就職作品/Assets/GameController.cs
+++ 就職作品/Assets/GameController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class GameController : MonoBehaviour
 {
     float limitTime;
+    public Sprite sprite;
 
     // Start is called before the first frame update
     void Start()
